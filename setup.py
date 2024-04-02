283aa2c62edebe5e7a23d37f802d2bbc02f4716b
Arian Hadi <arianhadi2003@gmail.com>
2024-04-03 00:18:17 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name = "fixer-demo",
+      version = '0.1',
+      description = 'getting rates with the help of fixer' ,
+      url = 'https://github.com/arian-hadi/fixer-Demo.git',
+      author = 'Arian Hadi',
+      author_email = 'arianhadi2003@gmail.com',
+      license = 'MIT',
+      packages = ['fixer'],
+      zip_safe = False)
