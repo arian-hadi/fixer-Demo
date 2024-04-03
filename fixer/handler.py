f5e086328c3b15c5670a04860c064aede404d334
Arian Hadi <arianhadi2003@gmail.com>
2024-04-03 19:45:16 +0300

    Bump to version 2

--- fixer/handler.py
+++ fixer/handler.py
@@ -1,2 +1,10 @@
-def get_rates():
-    print("Get rates touched")
+import requests
+import json
+
+API_URL = "http://data.fixer.io/api/latest?access_key="
+
+def get_rates(api_key):
+    response = requests.get(API_URL + api_key)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return None
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 setup(name = "fixer-demo",
-      version = '0.1',
+      version = '0.2',
       description = 'getting rates with the help of fixer' ,
       url = 'https://github.com/arian-hadi/fixer-Demo.git',
       author = 'Arian Hadi',
       author_email = 'arianhadi2003@gmail.com',
       license = 'MIT',
       packages = ['fixer'],
+      install_requires = ['requests'],
       zip_safe = False)
